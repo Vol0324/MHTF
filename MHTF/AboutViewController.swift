50636a0886394678ed02a906651425920281e949
Vol0324 <flight0324@yahoo.com>
2019-02-16 22:53:46 -0500

    First Ever Commit

---
+++ MHTF/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.swift
+//  MHTF
+//
+//  Created by Alex Zhou on 2/16/19.
+//  Copyright © 2019 MHTF. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var aboutLabel: UILabel!
+    @IBOutlet weak var aboutText: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func changeTitleForm(){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
